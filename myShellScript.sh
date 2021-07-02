dc959c005f0bb91a91a65f20122cdde023da2c6e
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-07-02 03:32:13 -0500

    input

--- myShellScript.sh
+++ myShellScript.sh
@@ -9,3 +9,11 @@ echo $myReadOnlyVar
 while read f
         do echo $f
 done < myShellScript.sh
+
+echo "What is your name?"
+read name
+echo "What is your pretend password?"
+read -s pass
+read -p "Animal? " animal
+
+echo "name: $name, pass: $pass, animal: $animal"
