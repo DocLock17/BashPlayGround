6630d26acaeac1fe573c6c8f6b4ce73bffc1d77c
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-07-02 07:30:39 -0500

    Update myShellScript.sh

--- myShellScript.sh
+++ myShellScript.sh
@@ -7,7 +7,7 @@ decalre -r myReadOnlyVar="This Variable is Read-Only"
 echo $myReadOnlyVar
 
 while read f
-        do echo $f
+	do echo $f
 done < myShellScript.sh
 
 echo "What is your name?"
@@ -17,3 +17,25 @@ read -s pass
 read -p "Animal? " animal
 
 echo "name: $name, pass: $pass, animal: $animal"
+
+read -p "Who am I? " server_name
+while [[ -z $server_name ]]
+do
+	read -p "Who am I? " server_name
+done
+
+echo "My server name is: $server_name"
+
+echo "What is my system type? "
+select sys_type in "Ubuntu-ML" "Ubuntu-Miner" "Ubuntu-Server" "Raspian-Desktop" "Raspian-LockCam" "Raspian-Console" "Raspian-Robot"
+
+#read -p "test" sys_type2
+#until [[ $sys_type2 =~ [0-9]{1} ]]; do
+#	read -p "toast" sys_type2
+#	select sys_type in "Ubuntu-ML" "Ubuntu-Miner" "Ubuntu-Server" "Raspian-Desktop" "Raspian-LockCam" "Raspian-Console"
+#done
+
+do
+	echo "You selected $sys_type"
+	break
+done
