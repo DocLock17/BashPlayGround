a3a0fff504dfc18327659175cca3f4678683c4ca
doclock17 <michael.c.locker@gmail.com>
2021-07-05 03:04:55 -0500

    ger

--- installSuite.sh
+++ installSuite.sh
@@ -96,15 +96,10 @@ install_ubuntu_nvidiaDrivers() {
 	echo "Installing Additional Packages" && \
 	echo " "
     # Install development and runtime libraries (~4GB)
-	sudo apt-get install --no-install-recommends \
-    cuda-11-0 \
-    libcudnn8=8.0.4.30-1+cuda11.0  \
-    libcudnn8-dev=8.0.4.30-1+cuda11.0
+	sudo apt-get install --no-install-recommends cuda-11-0 libcudnn8=8.0.4.30-1+cuda11.0 libcudnn8-dev=8.0.4.30-1+cuda11.0
 
 	# Install TensorRT. Requires that libcudnn8 is installed above.
-	sudo apt-get install -y --no-install-recommends libnvinfer7=7.1.3-1+cuda11.0 \
-    libnvinfer-dev=7.1.3-1+cuda11.0 \
-    libnvinfer-plugin7=7.1.3-1+cuda11.0
+	sudo apt-get install -y --no-install-recommends libnvinfer7=7.1.3-1+cuda11.0 libnvinfer-dev=7.1.3-1+cuda11.0 libnvinfer-plugin7=7.1.3-1+cuda11.0
 
 	sudo apt-get install nvidia-gds -y
 	echo " "
