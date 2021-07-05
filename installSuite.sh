fbe974d176036bcc0a742cceeff786cc20c346c1
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-07-04 21:59:22 -0500

    suggestions

--- installSuite.sh
+++ installSuite.sh
@@ -298,13 +298,16 @@ install_ubuntu_dependencies() {
 	## Install Dependency Libraries and Utilities
 	sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y  
 
-	sudo apt-get install curl gvfs gvfs-common gvfs-daemons gvfs-libs gconf-service gconf2 gconf2-common -y  
+	sudo apt-get install curl gvfs gvfs-common gvfs-daemons gvfs-libs gconf-service gconf2 gconf2-common gconf-defaults-service -y  
 
 	sudo apt-get install gvfs-bin psmisc libpango1.0-0 pciutils xclip xsel figlet cmake -y 
+	
+	sudo apt-get install binutils-doc cmake-doc ninja-build gcc-multilib autoconf automake libtool flex bison gcc-doc
+  gcc-9-multilib gcc-9-doc gcc-9-locales glibc-doc make-doc python-nautilus fancontrol read-edid i2c-tools libdirectfb-extra -y
 
 	#sudo apt install xfce4 xfce4-goodies -y
 	# node.js
-
+	echo " "
 	echo "Dependecies Installed"
 	echo " "
 	
