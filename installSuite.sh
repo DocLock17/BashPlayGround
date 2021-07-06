2d8e1afc2bc2963981ccfddef1824cd65a90ee44
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-07-05 21:56:39 -0500

    Hopefully it finally works

--- installSuite.sh
+++ installSuite.sh
@@ -219,47 +219,88 @@ install_ubuntu_nvidiaDrivers_Google_recommeded() {
 	# Install TensorRT. Requires that libcudnn8 is installed above.
 	sudo apt-get install -y --no-install-recommends libnvinfer7=7.1.3-1+cuda11.0 \
 		libnvinfer-dev=7.1.3-1+cuda11.0 \
-		libnvinfer-plugin7=7.1.3-1+cuda11.0 -y
+		libnvinfer-plugin7=7.1.3-1+cuda11.0 -y #7.2.3
 	echo " "
 	echo "Installation Complete " 
 	echo " "
 }
+# Nvidia driver-460 & CUDA-11.2 & cuDNN-8.1.1
 install_ubuntu_nvidiaDrivers() {
 	# Perhaps we should purge any previous pieces
 	#sudo apt-get --purge remove "*cublas*" "*cufft*" "*curand*" "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*" -y
 	#sudo apt-get --purge remove "*nvidia*" -y
 	#sudo apt-get autoremove -y
 	
+# 	sudo apt-get nvidia-driver-460 -y
+	
+	# Nvidia driver-460 & CUDA-11.2 & cuDNN-8.1.1
 	echo " "
 	echo "Add NVIDIA package repositories"
 	echo " "
-	# Add NVIDIA package repositories
+	# Add MAIN NVIDIA package repositories
 	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 	
-	wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.0-460.32.03-1_amd64.deb
-	sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.0-460.32.03-1_amd64.deb
+	# Add Selected CUDA Installation
+	#11.2 <-460
+	wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.0-460.27.04-1_amd64.deb
+	sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.0-460.27.04-1_amd64.deb
 	sudo apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub
-	sudo apt-get update
-	sudo apt-get -y install cuda
 	
+# 	#11.4 <-470 <-8.2.1
+# 	wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.0-470.42.01-1_amd64.deb
+# 	sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.0-470.42.01-1_amd64.deb
 
-	wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
-	sudo apt install ./nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
+
 	sudo apt-get update
+	sudo apt-get -y install cuda -y
 	
-	wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libnvinfer7_7.1.3-1+cuda11.2_amd64.deb
-	sudo apt install ./libnvinfer7_7.1.3-1+cuda11.2_amd64.deb
+	# Add Selected cuDNN Installation
+	# secure cuDNN
+	#https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.1.1.33/11.2_20210301/ubuntu20_04-x64/libcudnn8_8.1.1.33-1%2Bcuda11.2_amd64.deb
+	# Network ?
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin 
+	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+	sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+	
 	sudo apt-get update
+	sudo apt-get install libcudnn8=8.1.1.33-1+cuda11.2 -y
+	sudo apt-get install libcudnn8-dev=8.1.1.33-1+cuda11.2 -y
 	
 	echo " "
-	echo "Install development and runtime libraries (~4GB) "
+	echo "Adding cuda-11 to the PATH ..." && \
 	echo " "
-	# Install development and runtime libraries (~4GB)
-	sudo apt-get install --no-install-recommends \
-		cuda-11-2 \
-		libcudnn8.1  \
-		libcudnn8.1-dev -y
+	# Add CUDA to the PATH
+	echo """
+	# Nvidia driver-460 & CUDA-11.2 & cuDNN-8.1.1 PATH
+	export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
+
+	""" >> ~/.bashrc && \
+	echo " "
+
+# 	wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
+# 	sudo apt install ./nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
+# 	sudo apt-get update
+	
+# 	wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+# 	sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+# 	sudo apt-get update
+	
+# 	# for tensor RT
+# # 	wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libnvinfer7_7.1.3-1+cuda11.2_amd64.deb
+# # 	sudo apt install ./libnvinfer7_7.1.3-1+cuda11.2_amd64.deb
+# # 	sudo apt-get update
+	
+# 	echo " "
+# 	echo "Install development and runtime libraries (~4GB) "
+# 	echo " "
+# # 	# Install development and runtime libraries (~4GB)
+# # 	sudo apt-get install --no-install-recommends \
+# # 		cuda-11-2 \
+# # 		libcudnn8.0.4.30  \
+# # 		libcudnn8.0.4.30-dev -y
+	
 
 	echo " "
 	echo "Reboot. Check that GPUs are visible using the command: nvidia-smi "
@@ -268,16 +309,16 @@ install_ubuntu_nvidiaDrivers() {
 	echo " "
 }
 runMe(){
-	echo " "
-	echo "Adding cuda-11 to the PATH ..." && \
-	echo " "
-	# Add CUDA to the PATH
-	echo """
-	# Nvidia 465/70+11 PATH
-	export PATH=/usr/local/cuda-11.4/bin${PATH:+:${PATH}}
+# 	echo " "
+# 	echo "Adding cuda-11 to the PATH ..." && \
+# 	echo " "
+# 	# Add CUDA to the PATH
+# 	echo """
+# 	# Nvidia 465/70+11 PATH
+# 	export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
 
-	""" >> ~/.bashrc && \
-	echo " "
+# 	""" >> ~/.bashrc && \
+# 	echo " "
 }
 
 install_ubuntu_server(){
